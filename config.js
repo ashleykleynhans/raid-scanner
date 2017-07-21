b36e9b650ef2c3c2f9d3182996ab5915777d5a42
Ashley Kleynhans <ashley@digigoblin.com>
2017-07-21 18:45:55 +0200

    Update to API 0.69

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     password: 'INSERT_PASSWORD',
     hashingKey: 'INSERT_HASH_KEY',
     proxy: '', // Optional - leave empty for no proxy
-    appVersion: 6702
+    appVersion: 6900
   },
 
   telegram: {
